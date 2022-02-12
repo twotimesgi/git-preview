6341e641cb1f584906bd8dcb4d7893e3cbec62b7
twotimesgi <luigibardellagerbi@mail.com>
2022-02-12 11:33:06 +0100

    Php

--- preview.php
+++ preview.php
@@ -2,17 +2,51 @@
 
 if (isset($_GET["url"])){
     $raw = str_replace("/blob", "", str_replace("https://github.com/","https://raw.githubusercontent.com/", $_GET["url"]));
-    // $base_url = preg_match('/https:\/\/github.com\/.*\/.*\//', $raw);
     $s = explode("/", $raw);
     $base_url = str_replace($s[count($s) - 2]."/".$s[count($s) - 1],"", $raw);
-    $source = file_get_contents($raw);
+    $html = file_get_contents($raw);
+
+    //IMG
+    $htmlDom = new DOMDocument;
+    @$htmlDom->loadHTML($html);
+    $links = $htmlDom->getElementsByTagName('img');
+    $extractedLinks = array();
+    $newLinks = array();
+
+    foreach($links as $link){
+    $linkSrc = $link->getAttribute('src');
+    $extractedLinks[] = $linkSrc;
+    $newLinks[] = $base_url . "master/" . $linkSrc;
+    }
+
+    for ($i = 0; $i <= count($extractedLinks) - 1; $i++) {
+        $html = str_replace($extractedLinks[$i], $newLinks[$i], $html);
+    }
     
-    $source = str_replace("src=\"","src=\"".$base_url."master/",$source);
-    $source = str_replace("href=\"","href=\"".$base_url."master/",$source);
+    //IMG
+    $htmlDom = new DOMDocument;
+    @$htmlDom->loadHTML($html);
+    $links = $htmlDom->getElementsByTagName('link');
+    $extractedLinks = array();
+    $newLinks = array();
 
-    echo $base_url;
-    echo "https://raw.githubusercontent.com/twotimesgi/EsercizioLim/main/style.css";
+    foreach($links as $link){
+    $linkSrc = $link->getAttribute('href');
+    $extractedLinks[] = $linkSrc;
+    $newLinks[] = $base_url . "master/" . $linkSrc;
+    }
 
-    echo $source;
+    for ($i = 0; $i <= count($extractedLinks) - 1; $i++) {
+        if (str_contains($extractedLinks[$i],"http://") or str_contains($extractedLinks[$i],"https://"))  {
+            continue;
+        }
+        $html = str_replace($extractedLinks[$i], $newLinks[$i], $html);
+        echo "<style>".file_get_contents($newLinks[$i])."</style>";
+    }
+
+    echo $html;
+
 }
+
+
 ?>
